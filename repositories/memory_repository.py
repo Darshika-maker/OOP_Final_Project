e4a38f5798bd62a3b27d671e79a0cbd840eefe1d
Darshika-maker <darshikagamlath@outlook.com>
2026-02-23 22:44:16 +0200

    memory_repository.py

--- repositories/memory_repository.py
+++ repositories/memory_repository.py
@@ -2,13 +2,14 @@ from models.student import Student
 from models.course import Course
 from repositories.student_repository import StudentRepository
 
-
 class InMemoryStudentRepository(StudentRepository):
 
     def __init__(self):
         self._students = self._create_students()
 
     def _create_students(self):
+
+        
         s1 = Student("James Bond", "S001", "James1")
         s1.add_course(Course("Career Planning and Development 1", 2, 3))
         s1.add_course(Course("ICT Business", 5, 3))
@@ -59,19 +60,7 @@ class InMemoryStudentRepository(StudentRepository):
         s5.add_course(Course("Finnish 2", 5, 0, "upcoming"))
         s5.add_course(Course("OOP", 5, 0, "upcoming"))
 
-        s6 = Student("Sharma patel", "S006", "Sharma6")
-        s6.add_course(Course("Career Planning and Development 1", 2, 5))
-        s6.add_course(Course("ICT Business", 5, 4))
-        s6.add_course(Course("Finnish 1", 5, 3))
-        s6.add_course(Course("Data Management Systems 1",5,2))
-        s6.add_course(Course("Digital Technology Essentials",5, 5))
-        s6.add_course(Course("Web programming", 5, 0, "upcoming"))
-        s6.add_course(Course("Finnish 2", 5, 0, "upcoming"))
-        s6.add_course(Course("OOP", 5, 0, "upcoming"))
-
-        
-
-        return [s1, s2, s3, s4, s5, s6]
+        return [s1, s2, s3, s4, s5]
 
     def find_by_id(self, student_id: str):
         for student in self._students:
