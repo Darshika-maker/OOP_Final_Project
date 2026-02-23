09fe72707a4bd291ef528e6d1e74c4622cbbfbdd
Darshika-maker <darshikagamlath@outlook.com>
2026-02-23 22:42:40 +0200

    Update course.py

--- models/course.py
+++ models/course.py
@@ -1,9 +1,9 @@
 class Course:
-    def __init__(self, name: str, credits: int, grade: float=0, status: str = "finished"):
+    def __init__(self, name: str, credits: int, grade: float = 0, status: str = "finished"):
         self._name = name
         self._credits = credits
         self._grade = grade
-        self._status = status
+        self._status = status  # "finished" or "upcoming"
 
     def get_name(self):
         return self._name
@@ -20,4 +20,4 @@ class Course:
     def __str__(self):
         if self._status == "upcoming":
             return f"{self._name} | Credits: {self._credits} | [UPCOMING]"
-        return f"{self._name} | Credits: {self._credits} | Grade: {self._grade}" 
+        return f"{self._name} | Credits: {self._credits} | Grade: {self._grade}"
